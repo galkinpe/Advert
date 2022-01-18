7f8da0078d3ad8ce0692693fdd8602933928cbb3
Your Name <obor528622@gmail.com>
2022-01-18 17:57:08 +0700

    asd2

--- task/Adverts/models.py
+++ task/Adverts/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 
 class Category(models.Model):
-    name =  models.CharField(max_length=200,)
+    name =  models.CharField(max_length=200, verbose_name = 'Категория')
+
+    def __str__(self):
+        return self.name
     
     class Meta:
         verbose_name = 'Категория'
@@ -11,6 +14,9 @@ class Category(models.Model):
 
 class City(models.Model):
     name =  models.CharField(max_length=200,)
+
+    def __str__(self):
+        return self.name
 
 
     class Meta:
--- task/Adverts/views.py
+++ task/Adverts/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Advert
 
 
 def index(request):
-    advert = Advert.object.all()
-    context = {
-        
+    adverts = Advert.objects.all()
+    context = {'adverts': adverts, 
+               'title': 'Список новостей'
     }
+    return render(request,'adverts/index.html',context) 
+
+
+
 
 
